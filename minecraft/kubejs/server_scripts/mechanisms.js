3b128b61a785120c1d53c082316232c483399806
omgimanerd <alvin@omgimanerd.tech>
2024-04-01 06:30:06 -0400

    Added SequencedAssembly class for easier mechanism registration

--- minecraft/kubejs/server_scripts/mechanisms.js
+++ minecraft/kubejs/server_scripts/mechanisms.js
@@ -2,72 +2,95 @@
 
 ServerEvents.recipes(e => {
 
-  let deployIntermediate = (intermediate, input) => {
-    return e.recipes.create.deploying(intermediate, [intermediate, input])
+  // JS prototype class to make registering mechanism sequenced assemblies
+  // easier.
+  function SequencedAssembly(input, intermediate) {
+    this.input = input
+    this.intermediate = intermediate
+
+    this.loops = 1
+    this.steps = []
+  }
+
+  SequencedAssembly.prototype.loops = function(n) {
+    this.loops = n
+    return this
+  }
+
+  SequencedAssembly.prototype.cut = function() {
+    this.steps.push({ cut: true })
+    return this
+  }
+
+  SequencedAssembly.prototype.press = function() {
+    this.steps.push({ press: true })
+    return this
+  }
+
+  SequencedAssembly.prototype.fill = function(fluid_id, qty_mb) {
+    this.steps.push({ fill: Fluid.of(Fluid.getId(fluid_id), qty_mb) })
+    return this
+  }
+
+  SequencedAssembly.prototype.deploy = function(item) {
+    this.steps.push({ deploy: item })
+    return this
+  }
+
+  SequencedAssembly.prototype.outputs = function(output) {
+    const steps = this.steps.map(step => {
+      if (step.cut !== undefined) {
+        return e.recipes.createPressing(this.intermediate, this.intermediate)
+      } else if (step.press !== undefined) {
+        return e.recipes.createPressing(this.intermediate, this.intermediate)
+      } else if (step.fill !== undefined) {
+        return e.recipes.createFilling(
+          this.intermediate, [this.intermediate, step.fill])
+      } else if (step.deploy !== undefined) {
+        return e.recipes.createDeploying(
+          this.intermediate, [this.intermediate, step.deploy])
+      } else {
+        throw new Error('Unknown SequencedAssembly step!')
+      }
+    })
+    e.recipes.create.sequenced_assembly(
+        [output], this.input, steps)
+      .transitionalItem(this.intermediate)
+      .loops(this.loops)
   }
 
   // Andesite Mechanism
-  e.recipes.create.sequenced_assembly(
-    ['kubejs:andesite_mechanism'],  // output
-    'create:andesite_alloy',  // input
-    [
-      deployIntermediate(
-        'kubejs:incomplete_andesite_mechanism', '#minecraft:planks'),
-      deployIntermediate(
-        'kubejs:incomplete_andesite_mechanism', 'create:shaft'),
-      deployIntermediate(
-        'kubejs:incomplete_andesite_mechanism', 'create:cogwheel'),
-    ],
-  ).transitionalItem('kubejs:incomplete_andesite_mechanism').loops(1)
+  new SequencedAssembly('create:andesite_alloy',
+                        'kubejs:incomplete_andesite_mechanism')
+    .deploy('#minecraft:planks')
+    .deploy('create:shaft')
+    .deploy('create:cogwheel')
+    .outputs('kubejs:andesite_mechanism')
 
   // Copper Mechanism
-  e.recipes.create.sequenced_assembly(
-    ['kubejs:copper_mechanism'],  // output
-    'kubejs:andesite_mechanism',  // input
-    [
-      deployIntermediate(
-        'kubejs:incomplete_copper_mechanism', 'create:copper_sheet'),
-      deployIntermediate(
-        'kubejs:incomplete_copper_mechanism', 'thermal:cured_rubber'),
-      e.recipes.createPressing(
-        'kubejs:incomplete_copper_mechanism', 'kubejs:incomplete_copper_mechanism'),
-    ],
-  ).transitionalItem('kubejs:incomplete_copper_mechanism').loops(1)
+  new SequencedAssembly('kubejs:andesite_mechanism',
+                        'kubejs:incomplete_copper_mechanism')
+    .deploy('create:copper_sheet')
+    .deploy('thermal:cured_rubber')
+    .press()
+    .outputs('kubejs:copper_mechanism')
 
   // Source Mechanism
-  e.recipes.create.sequenced_assembly(
-    ['kubejs:source_mechanism'],  // output
-    'kubejs:andesite_mechanism',  // input
-    [
-      deployIntermediate(
-        'kubejs:incomplete_source_mechanism', 'ars_nouveau:source_gem'),
-      e.recipes.createPressing(
-        'kubejs:incomplete_source_mechanism', 'kubejs:incomplete_source_mechanism'),
-      e.recipes.createFilling(
-        'kubejs:incomplete_source_mechanism',
-        ['kubejs:incomplete_source_mechanism',
-          Fluid.of(Fluid.getId('starbunclemania:source_fluid'), 100)]
-      ),
-      e.recipes.createPressing(
-        'kubejs:incomplete_source_mechanism', 'kubejs:incomplete_source_mechanism'),
-    ],
-  ).transitionalItem('kubejs:incomplete_source_mechanism').loops(1)
+  new SequencedAssembly('kubejs:andesite_mechanism',
+                        'kubejs:incomplete_source_mechanism')
+    .deploy('ars_nouveau:source_gem')
+    .press()
+    .fill('starbunclemania:source_fluid', 100)
+    .press()
+    .outputs('kubejs:source_mechanism')
 
-  // Redo precision mechanism recipe
+  // Precision mechanism recipe
   e.remove({ output: 'create:precision_mechanism' })
-  e.recipes.create.sequenced_assembly(
-    ['create:precision_mechanism'],  // output
-    'kubejs:andesite_mechanism',  // input
-    [
-      deployIntermediate(
-        'create:incomplete_precision_mechanism', 'create:electron_tube'),
-      e.recipes.createPressing(
-        'create:incomplete_precision_mechanism', 'create:incomplete_precision_mechanism'),
-      deployIntermediate(
-        'create:incomplete_precision_mechanism', 'create:brass_sheet'),
-      e.recipes.createPressing(
-        'create:incomplete_precision_mechanism', 'create:incomplete_precision_mechanism'),
-    ],
-  ).transitionalItem('create:incomplete_precision_mechanism').loops(1)
-
+  new SequencedAssembly('kubejs:andesite_mechanism',
+                        'create:incomplete_precision_mechanism')
+    .deploy('create:electron_tube')
+    .press()
+    .deploy('create:brass_sheet')
+    .press()
+    .outputs('create:precision_mechanism')
 })
