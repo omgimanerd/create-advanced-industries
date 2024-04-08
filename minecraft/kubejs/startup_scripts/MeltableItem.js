dda52f9c62ec7d6f615cd46032251d804425e855
omgimanerd <alvin@omgimanerd.tech>
2024-04-08 05:52:03 -0400

    Fix a bug with noGemRecipes splashing recipes.

--- minecraft/kubejs/startup_scripts/MeltableItem.js
+++ minecraft/kubejs/startup_scripts/MeltableItem.js
@@ -24,18 +24,18 @@ function MeltableItem(options) {
   this.clayCastItem = this.isGem
     ? 'kubejs:clay_gem_cast'
     : 'kubejs:clay_ingot_cast'
-  this.clayBlockCastItem = 'kubejs:clay_block_cast'
   this.steelCastItem = this.isGem
     ? 'kubejs:steel_gem_cast'
     : 'kubejs:steel_ingot_cast'
+  this.clayBlockCastItem = 'kubejs:clay_block_cast'
   this.steelBlockCastItem = 'kubejs:steel_block_cast'
 
   // Names of the items that result from having the molten liquids poured into
   // them.
   const strippedName = stripPrefix(this.fluid)
   this.castedClayCastItem = `kubejs:${strippedName}_clay_cast`
-  this.castedClayBlockCastItem = `kubejs:${strippedName}_block_clay_cast`
   this.castedSteelCastItem = `kubejs:${strippedName}_steel_cast`
+  this.castedClayBlockCastItem = `kubejs:${strippedName}_block_clay_cast`
   this.castedSteelBlockCastItem = `kubejs:${strippedName}_steel_block_cast`
 }
 
@@ -82,17 +82,6 @@ MeltableItem.prototype.registerCastedItems = function (e) {
       .color(1, this.fluidColor)
       .displayName(`Claycast ${fluidDisplayName}`)
       .maxStackSize(16)
-  }
-  e.create(this.castedClayBlockCastItem)
-    .textureJson({
-      layer0: MeltableItem.BASE_CAST_TEXTURE,
-      layer1: `${global.cai}:item/block_cast_negative`,
-    })
-    .color(0, MeltableItem.CLAY_CAST_COLOR)
-    .color(1, this.fluidColor)
-    .displayName(`Claycast ${fluidDisplayName} Block`)
-    .maxStackSize(16)
-  if (!this.noGemCastingRecipe) {
     e.create(this.castedSteelCastItem)
       .textureJson({
         layer0: MeltableItem.BASE_CAST_TEXTURE,
@@ -103,6 +92,15 @@ MeltableItem.prototype.registerCastedItems = function (e) {
       .displayName(`Steelcast ${fluidDisplayName}`)
       .maxStackSize(16)
   }
+  e.create(this.castedClayBlockCastItem)
+    .textureJson({
+      layer0: MeltableItem.BASE_CAST_TEXTURE,
+      layer1: `${global.cai}:item/block_cast_negative`,
+    })
+    .color(0, MeltableItem.CLAY_CAST_COLOR)
+    .color(1, this.fluidColor)
+    .displayName(`Claycast ${fluidDisplayName} Block`)
+    .maxStackSize(16)
   e.create(this.castedSteelBlockCastItem)
     .textureJson({
       layer0: MeltableItem.BASE_CAST_TEXTURE,
@@ -162,17 +160,15 @@ MeltableItem.prototype.registerCastingRecipes = function (e) {
       Fluid.of(this.fluid, MeltableItem.DEFAULT_INGOT_FLUID),
       this.clayCastItem,
     ])
-  }
-  e.recipes.create.filling(this.castedClayBlockCastItem, [
-    Fluid.of(this.fluid, MeltableItem.DEFAULT_INGOT_FLUID * this.blockRatio),
-    this.clayBlockCastItem,
-  ])
-  if (!this.noGemCastingRecipe) {
     e.recipes.create.filling(this.castedSteelCastItem, [
       Fluid.of(this.fluid, MeltableItem.DEFAULT_INGOT_FLUID),
       this.steelCastItem,
     ])
   }
+  e.recipes.create.filling(this.castedClayBlockCastItem, [
+    Fluid.of(this.fluid, MeltableItem.DEFAULT_INGOT_FLUID * this.blockRatio),
+    this.clayBlockCastItem,
+  ])
   e.recipes.create.filling(this.castedSteelBlockCastItem, [
     Fluid.of(this.fluid, MeltableItem.DEFAULT_INGOT_FLUID * this.blockRatio),
     this.steelBlockCastItem,
@@ -184,15 +180,21 @@ MeltableItem.prototype.registerCastingRecipes = function (e) {
 //
 // Can only be called in server_scripts/
 MeltableItem.prototype.registerWashedCastRecipes = function (e) {
-  e.recipes.create.splashing(
-    [
-      this.ingot,
-      Item.of(this.clayCastItem).withChance(
-        MeltableItem.CLAY_CAST_RETURN_CHANCE
-      ),
-    ],
-    this.castedClayCastItem
-  )
+  if (!this.noGemCastingRecipe) {
+    e.recipes.create.splashing(
+      [
+        this.ingot,
+        Item.of(this.clayCastItem).withChance(
+          MeltableItem.CLAY_CAST_RETURN_CHANCE
+        ),
+      ],
+      this.castedClayCastItem
+    )
+    e.recipes.create.splashing(
+      [this.ingot, this.steelCastItem],
+      this.castedSteelCastItem
+    )
+  }
   e.recipes.create.splashing(
     [
       this.blockCastingOutput ? this.blockCastingOutput : this.block,
@@ -201,10 +203,6 @@ MeltableItem.prototype.registerWashedCastRecipes = function (e) {
       ),
     ],
     this.castedClayBlockCastItem
-  )
-  e.recipes.create.splashing(
-    [this.ingot, this.steelCastItem],
-    this.castedSteelCastItem
   )
   e.recipes.create.splashing(
     [
