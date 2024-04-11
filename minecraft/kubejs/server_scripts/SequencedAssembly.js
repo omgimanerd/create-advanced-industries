79d4fc59855de876ce5d9e57797f40208d633494
omgimanerd <alvin@omgimanerd.tech>
2024-04-11 16:58:35 -0400

    Change SequencedAssembly to use var_ internals

--- minecraft/kubejs/server_scripts/SequencedAssembly.js
+++ minecraft/kubejs/server_scripts/SequencedAssembly.js
@@ -2,66 +2,74 @@
 
 // JS prototype class to make registering mechanism sequenced assemblies
 // easier.
+//
+// Example usage:
+// new SequencedAssembly('#minecraft:wooden_slabs')
+//   .transitional('custom:incomplete_andesite_mechanism')
+//   .deploy('create:andesite_alloy')
+//   .deploy('create:shaft')
+//   .deploy('create:cogwheel')
+//   .outputs(e, 'kubejs:andesite_mechanism')
 function SequencedAssembly(input) {
-  this.input = input
-  this.intermediate = input
+  this.input_ = input
+  this.intermediate_ = input
 
-  this.nLoops = 1
-  this.steps = []
+  this.loops_ = 1
+  this.steps_ = []
 }
 
 SequencedAssembly.prototype.transitional = function (intermediate) {
-  this.intermediate = intermediate
+  this.intermediate_ = intermediate
   return this
 }
 
-SequencedAssembly.prototype.loops = function (n) {
-  this.nLoops = n
+SequencedAssembly.prototype.loops = function (loops) {
+  this.loops_ = loops
   return this
 }
 
 SequencedAssembly.prototype.cut = function (repeats) {
   if (repeats === undefined) repeats = 1
-  this.steps = this.steps.concat(Array(repeats).fill({ cut: true }))
+  this.steps_ = this.steps_.concat(Array(repeats).fill({ cut: true }))
   return this
 }
 
 SequencedAssembly.prototype.press = function (repeats) {
   if (repeats === undefined) repeats = 1
-  this.steps = this.steps.concat(Array(repeats).fill({ press: true }))
+  this.steps_ = this.steps_.concat(Array(repeats).fill({ press: true }))
   return this
 }
 
 SequencedAssembly.prototype.fill = function (fluid, qty_mb) {
   // 1-argument, Fluid object is provided.
   if (qty_mb === undefined) {
-    this.steps.push({ fill: fluid })
+    this.steps_.push({ fill: fluid })
   } else {
     // 2-argument, fluid id + quantity in mb
-    this.steps.push({ fill: Fluid.of(fluid, qty_mb) })
+    this.steps_.push({ fill: Fluid.of(fluid, qty_mb) })
   }
   return this
 }
 
 SequencedAssembly.prototype.deploy = function (item, keepHeldItem) {
-  this.steps.push({ deploy: item, keepHeldItem: !!keepHeldItem })
+  this.steps_.push({ deploy: item, keepHeldItem: !!keepHeldItem })
   return this
 }
 
 SequencedAssembly.prototype.outputs = function (e, output) {
-  const steps = this.steps.map((step) => {
+  const steps = this.steps_.map((step) => {
     if (step.cut !== undefined) {
-      return e.recipes.createPressing(this.intermediate, this.intermediate)
+      return e.recipes.createPressing(this.intermediate_, this.intermediate_)
     } else if (step.press !== undefined) {
-      return e.recipes.createPressing(this.intermediate, this.intermediate)
+      return e.recipes.createPressing(this.intermediate_, this.intermediate_)
     } else if (step.fill !== undefined) {
-      return e.recipes.createFilling(this.intermediate, [
-        this.intermediate,
+      return e.recipes.createFilling(this.intermediate_, [
+        this.intermediate_,
         step.fill,
       ])
     } else if (step.deploy !== undefined) {
-      let r = e.recipes.createDeploying(this.intermediate, [
-        this.intermediate,
+      let r = e.recipes.createDeploying(this.intermediate_, [
+        this.intermediate_,
         step.deploy,
       ])
       if (step.keepHeldItem) {
@@ -74,7 +82,7 @@ SequencedAssembly.prototype.outputs = function (e, output) {
   })
   const outputArray = typeof output === 'string' ? [output] : output
   return e.recipes.create
-    .sequenced_assembly(outputArray, this.input, steps)
-    .transitionalItem(this.intermediate)
-    .loops(this.nLoops)
+    .sequenced_assembly(outputArray, this.input_, steps)
+    .transitionalItem(this.intermediate_)
+    .loops(this.loops_)
 }
