d41910960181ba1bba1b9b2fc05da010b11e3a0e
omgimanerd <alvin@omgimanerd.tech>
2024-04-11 23:47:12 -0400

    Implement PressureChamberRecipe wrapper for pneumaticcraft.

---
+++ minecraft/kubejs/server_scripts/pneumaticcraft/PressureChamberRecipe.js
@@ -0,0 +1,47 @@
+// priority: 900
+
+// JS prototype class to make registering Pneumaticcraft Pressure Chamber
+// recipes easier.
+
+function PressureChamberRecipe(e, inputs) {
+  this.e_ = e
+  this.inputs_ = inputs
+
+  this.pressure_ = 1.0
+}
+
+PressureChamberRecipe.RECIPE_TYPE = 'pneumaticcraft:pressure_chamber'
+
+PressureChamberRecipe.prototype.pressure = function (pressure) {
+  this.pressure_ = pressure
+  return this
+}
+
+PressureChamberRecipe.prototype.outputs = function (outputs) {
+  let base = {
+    type: PressureChamberRecipe.RECIPE_TYPE,
+    pressure: this.pressure_,
+    inputs: [],
+    results: [],
+  }
+
+  this.inputs_ = Array.isArray(this.inputs_) ? this.inputs_ : [this.inputs_]
+  for (const input of this.inputs_) {
+    const g = PneumaticcraftUtils.parseStackedItemInput(input)
+    if (g === null) throw new Error(`Invalid input ${input}`)
+    base.inputs.push(g)
+  }
+
+  outputs = Array.isArray(outputs) ? outputs : [outputs]
+  for (const output of outputs) {
+    const g = PneumaticcraftUtils.parseItemOutput(output)
+    if (g === null) throw new Error(`Invalid output ${output}`)
+    base.results.push(g)
+  }
+
+  if (PneumaticcraftUtils.LOG_DEBUG_OUTPUT) {
+    console.log(JSON.stringify(base))
+  }
+
+  return this.e_.custom(base)
+}
