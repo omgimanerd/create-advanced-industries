60601d89cdda4d9c6b6cfc8696373c19342a33bd
omgimanerd <alvin@omgimanerd.tech>
2024-04-13 04:26:34 -0400

    Add a custom recipe wrapper for Pneumaticcraft's special shaped recipes.

--- minecraft/kubejs/server_scripts/pneumaticcraft/definePneumaticcraftRecipes.js
+++ minecraft/kubejs/server_scripts/pneumaticcraft/definePneumaticcraftRecipes.js
@@ -1,5 +1,36 @@
 // priority: 500
 
+// Helper to define the special custom shaped recipe formats used by
+// Pneumaticcraft
+const shapedSpecial = (e, type, output, pattern, keys) => {
+  const allowedTypes = {
+    'pneumaticcraft:compressor_upgrade_crafting': true,
+    'pneumaticcraft:crafting_shaped_no_mirror': true,
+    'pneumaticcraft:crafting_shaped_pressurizable': true,
+  }
+  if (!(type in allowedTypes)) throw new Error(`Unknown type ${type}`)
+  const base = {
+    type: type,
+    category: 'misc',
+    pattern: pattern,
+    key: {},
+  }
+  const keysInPattern = pattern.join('')
+  for (const [key, item] of Object.entries(keys)) {
+    if (!keysInPattern.includes(key)) continue
+    let parsedItem = Parser.parseItemInput(item)
+    if (!setIfValid(base.key, key, parsedItem)) {
+      throw new Error(`Invalid keys: ${keys}`)
+    }
+  }
+  const outputItem = Parser.parseItemOutput(output)
+  if (!setIfValid(base, 'result', outputItem)) {
+    throw new Error(`Invalid output ${output}`)
+  }
+
+  return e.custom(base)
+}
+
 /**
  *
  * @param {dev.latvian.mods.kubejs.recipe.RecipeEventsJS} e
@@ -34,5 +65,12 @@ const definePneumaticcraftRecipes = (e) => {
      * @returns {ThermoPlant}
      */
     ThermoPlant: getConstructorWrapper(e, ThermoPlant),
+    /**
+     * @param {string} type
+     * @param {string} output
+     * @param {string[]} pattern
+     * @param {Object<string, string>} output
+     */
+    shapedSpecial: getPartialApplication(e, shapedSpecial),
   }
 }
