62c44d7af1ddf8b6c9c4bbfbffc295cac1e33e97
omgimanerd <alvin@omgimanerd.tech>
2024-04-24 19:47:31 -0400

    Overhaul Functional Storage drawer recipes to gate some content behind precision mechanisms.

--- minecraft/kubejs/server_scripts/overhauls/storage.js
+++ minecraft/kubejs/server_scripts/overhauls/storage.js
@@ -4,6 +4,66 @@
 ServerEvents.recipes((e) => {
   const create = defineCreateRecipes(e)
 
+  const redefineRecipe = redefineRecipe_(e)
+
+  ////////////////////////
+  // Functional Storage //
+  ////////////////////////
+  e.replaceInput(
+    [
+      {
+        id: 'functionalstorage:armory_cabinet',
+      },
+      { id: 'functionalstorage:storage_controller' },
+    ],
+    'minecraft:comparator',
+    'create:precision_mechanism'
+  )
+  redefineRecipe(
+    'functionalstorage:framed_storage_controller',
+    [
+      'NNN', //
+      'NCN', //
+      'NNN', //
+    ],
+    {
+      N: 'minecraft:iron_nugget',
+      C: 'functionalstorage:storage_controller',
+    }
+  )
+  redefineRecipe('functionalstorage:controller_extension', [
+    'functionalstorage:storage_controller',
+  ])
+  redefineRecipe(
+    'functionalstorage:framed_controller_extension',
+    [
+      'NNN', //
+      'NCN', //
+      'NNN', //
+    ],
+    {
+      N: 'minecraft:iron_nugget',
+      C: 'functionalstorage:controller_extension',
+    }
+  )
+  // TODO gate behind quantum mechanisms
+  e.remove({ id: 'functionalstorage:ender_drawer' })
+  e.replaceInput(
+    [
+      {
+        id: 'functionalstorage:collector_upgrade',
+      },
+      {
+        id: 'functionalstorage:pusher_upgrade',
+      },
+      {
+        id: 'functionalstorage:puller_upgrade',
+      },
+    ],
+    'minecraft:redstone',
+    'create:precision_mechanism'
+  )
+
   /////////////////////
   // Refined Storage //
   /////////////////////
