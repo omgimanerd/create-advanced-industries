28f3704b867334e347e04eff7be0919c1e59042d
omgimanerd <alvin.lin.dev@gmail.com>
2024-04-27 22:00:24 -0400

    Consolidate item registries in startup_scripts.

--- minecraft/kubejs/startup_scripts/items.js
+++ minecraft/kubejs/startup_scripts/items.js
@@ -1,7 +1,31 @@
-// priority: 10
+// priority: 1000
 
 StartupEvents.registry('item', (e) => {
   const registerItem = registerItem_(e)
+
+  const registerMechanism = (name) => {
+    registerItem(name)
+    registerItem(
+      `kubejs:incomplete_${stripPrefix(name)}`,
+      'create:sequenced_assembly'
+    )
+  }
+  registerMechanism('kubejs:andesite_mechanism')
+  registerMechanism('kubejs:copper_mechanism')
+  registerMechanism('kubejs:source_mechanism')
+  // Precision mechanism defined by Create
+  // Steel mechanism defined by TFMG
+
+  // Logistics Mechanism
+  e.create('kubejs:logistics_mechanism')
+    .texture(getResourceLocation('pneumaticcraft:logistics_core'))
+    .displayName('Logistics Mechanism')
+  e.create('kubejs:incomplete_logistics_mechanism', 'create:sequenced_assembly')
+    .texture(getResourceLocation('pneumaticcraft:logistics_core'))
+    .displayName('Incomplete Logistics Mechanism')
+
+  // TBD
+  registerMechanism('kubejs:plastic_mechanism')
 
   // Wooden hand used for crafting deployers
   registerItem('kubejs:wooden_hand')
