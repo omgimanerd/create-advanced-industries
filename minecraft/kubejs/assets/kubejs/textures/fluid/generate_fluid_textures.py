63a7b6d4e4b447b91482e28b959b759a433505da
omgimanerd <alvin@omgimanerd.tech>
2024-05-21 21:54:54 -0400

    Generate still and flowing textures for all custom molten metals

--- minecraft/kubejs/assets/kubejs/textures/fluid/generate_fluid_textures.py
+++ minecraft/kubejs/assets/kubejs/textures/fluid/generate_fluid_textures.py
@@ -13,32 +13,44 @@ def get_path(p):
 
 
 METAL_FLUIDS = {
-    "molten_iron.png": (90, 3, 3),
-    "molten_copper.png": (163, 59, 31),
-    "molten_gold.png": (236, 209, 41),
-    "molten_zinc.png": (174, 189, 168),
-    "molten_brass.png": (209, 156, 57),
-    "molten_lead.png": (38, 38, 83),
-    "molten_silver.png": (168, 184, 191),
-    "molten_glass.png": (206, 231, 230),
-    "molten_quartz.png": (158, 153, 153),
-    "molten_diamond.png": (30, 194, 195),
-    "molten_emerald.png": (22, 189, 110),
-    "molten_lapis.png": (44, 92, 200),
-    "molten_redstone.png": (135, 21, 21)
+    "molten_iron": (90, 3, 3),
+    "molten_copper": (163, 59, 31),
+    "molten_gold": (236, 209, 41),
+    "molten_zinc": (174, 189, 168),
+    "molten_brass": (209, 156, 57),
+    "molten_lead": (38, 38, 83),
+    "molten_silver": (168, 184, 191),
+    "molten_glass": (206, 231, 230),
+    "molten_quartz": (158, 153, 153),
+    "molten_diamond": (30, 194, 195),
+    "molten_emerald": (22, 189, 110),
+    "molten_lapis": (44, 92, 200),
+    "molten_redstone": (135, 21, 21)
 }
 
 
 if __name__ == '__main__':
 
-    template_fluid_img = Image.open(
-        get_path('template_fluid.png')).convert('RGB')
-    with open(get_path('template_fluid.png.mcmeta')) as f:
-        template_fluid_mcmeta = f.read()
+    template_fluid_still_img = Image.open(
+        get_path('template_fluid_still.png')).convert('RGB')
+    with open(get_path('template_fluid_still.png.mcmeta')) as f:
+        template_fluid_still_mcmeta = f.read()
+    template_fluid_flow_img = Image.open(
+        get_path('template_fluid_flow.png')).convert('RGB')
+    with open(get_path('template_fluid_flow.png.mcmeta')) as f:
+        template_fluid_flow_mcmeta = f.read()
 
     for (imagename, color) in METAL_FLUIDS.items():
-        img = RGBTransform().mix_with(color, factor=0.9).applied_to(
-            template_fluid_img)
-        img.save(get_path(imagename))
-        with open(get_path(f'{imagename}.mcmeta'), 'w') as f:
-            f.write(template_fluid_mcmeta)
+        still_img = RGBTransform().mix_with(color, factor=0.9).applied_to(
+            template_fluid_still_img)
+        still_img.save(get_path(f'{imagename}_still.png'))
+
+        with open(get_path(f'{imagename}_still.png.mcmeta'), 'w') as f:
+            f.write(template_fluid_still_mcmeta)
+
+        flow_img = RGBTransform().mix_with(color, factor=0.9).applied_to(
+            template_fluid_flow_img)
+        flow_img.save(get_path(f'{imagename}_flow.png'))
+
+        with open(get_path(f'{imagename}_flow.png.mcmeta'), 'w') as f:
+            f.write(template_fluid_flow_mcmeta)
