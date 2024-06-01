36884f9b6f500039c94f7a56bac8018cf243578f
omgimanerd <alvin@omgimanerd.tech>
2024-06-01 18:05:04 -0400

    Fix starting items stage check.

--- minecraft/kubejs/server_scripts/starting.js
+++ minecraft/kubejs/server_scripts/starting.js
@@ -1,7 +1,7 @@
 // priority: 100
 
 PlayerEvents.loggedIn((e) => {
-  if (e.player.stages.has('starting_items')) {
+  if (!e.player.stages.has('starting_items')) {
     e.player.stages.add('starting_items')
     e.player.inventory.clear()
     e.player.give('ftbquests:book')
