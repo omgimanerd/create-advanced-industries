fafdc8e92bb9c83999f3b516855695a4c4b21f6c
omgimanerd <alvin@omgimanerd.tech>
2024-06-02 23:07:57 -0400

    Add placeholder ponder file for infested stone expulsion.

---
+++ minecraft/kubejs/client_scripts/ponder/infestedStone.js
@@ -0,0 +1,11 @@
+// priority: 100
+
+Ponder.registry((e) => {
+  e.create('minecraft:infested_stone').scene(
+    'infested_stone_expulsion',
+    'Getting rid of Pests',
+    (scene, util) => {
+      // TODO: custom ponder to show infested stone expulsion mechanic
+    }
+  )
+})
