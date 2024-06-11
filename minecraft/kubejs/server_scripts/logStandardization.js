8b4655fcf0e9c0677287ad25617b65df8e631b50
omgimanerd <alvin@omgimanerd.tech>
2024-06-10 20:35:05 -0400

    Add a script to standardize log stripping and correctly tag all stripped logs.

---
+++ minecraft/kubejs/server_scripts/logStandardization.js
@@ -0,0 +1,70 @@
+// priority: 1000
+
+const STRIPPED_LOG_REGEX = /^[a-z_]*:stripped_[a-z_]*_(log|hyphae|stem|block)$/
+
+ServerEvents.tags('item', (e) => {
+  // As far as I can tell, this regex should match all stripped logs. Check if
+  // they are correctly tagged, otherwise tag them. The tag cannot be used in
+  // any KubeJS recipe script because tags and recipes are loaded in a
+  // nondeterministic order.
+  Ingredient.of(STRIPPED_LOG_REGEX).itemIds.forEach((id) => {
+    if (!Item.of(id).hasTag('forge:stripped_logs')) {
+      e.add('forge:stripped_logs', id)
+      console.log(`${id} was not tagged with forge:stripped_logs, adding tag.`)
+    }
+  })
+})
+
+ServerEvents.recipes((e) => {
+  const create = defineCreateRecipes(e)
+
+  const getLogIdFromStripped = (s) => {
+    return s.replace('stripped_', '')
+  }
+  // Store a mapping of all logs and stripped logs.
+  const logs = {}
+  const strippedLogs = {}
+  const mapping = {}
+  Ingredient.of(STRIPPED_LOG_REGEX).itemIds.forEach((id) => {
+    logs[getLogIdFromStripped(id)] = true
+    strippedLogs[id] = true
+    mapping[getLogIdFromStripped(id)] = id
+  })
+
+  // Validate the log to stripped log mapping, and that all unstripped logs are
+  // present in #minecraft:logs. This will work as long as #minecraft.logs has
+  // not been modified by KJS, again for tag/recipe load determinism reasons.
+  Ingredient.of('#minecraft:logs').itemIds.forEach((id) => {
+    if (id.endsWith('wood')) return
+    if (id in logs) return
+    if (id in strippedLogs) return
+    console.error(`${id} is in #minecraft:logs but does not have a mapping`)
+  })
+
+  // Remove and register cutting and stripping recipes for all of them.
+  Ingredient.of(STRIPPED_LOG_REGEX).itemIds.forEach((id) => {
+    const log = getLogIdFromStripped(id)
+    create
+      .item_application(id, [log, '#forge:tools/axes'])
+      .id(
+        `kubejs:${stripOutputPrefix(log)}_to_${stripOutputPrefix(id)}_stripping`
+      )
+
+    // Remove and add a cutting recipe
+    e.remove({ type: 'create:cutting', output: id })
+    create.cutting(id, log)
+
+    // Remove and add a Farmer's Delight cutting recipe for bark
+    e.remove({ type: 'farmersdelight:cutting', output: id })
+    e.recipes.farmersdelight.cutting(
+      log,
+      [id, 'farmersdelight:tree_bark'],
+      '#forge:tools/axes'
+    )
+
+    // Add a sanding recipe that yields sawdust
+    create
+      .polishing([id, Item.of('thermal:sawdust', 3)], log)
+      .speed_limits(POLISHING_MEDIUM_SPEED)
+  })
+})
