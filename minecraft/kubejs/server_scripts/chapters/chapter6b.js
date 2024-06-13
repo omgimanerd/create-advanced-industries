3f4f8ef86228113268dfcd143cab5cdd30c2a858
omgimanerd <alvin@omgimanerd.tech>
2024-06-12 22:32:57 -0400

    Move some tech recipes to a separated chapter 6a

--- minecraft/kubejs/server_scripts/chapters/chapter6b.js
+++ minecraft/kubejs/server_scripts/chapters/chapter6b.js
@@ -42,7 +42,7 @@ ServerEvents.recipes((e) => {
   const positions = global.shuffle(global.combinatorics(9, 4), rand)
   const gemData = Object.entries(apotheoticGems)
   if (gemData.length > positions.length) {
-    console.error('Not enough permutations to generate unique recipes!')
+    console.error('Not enough combinations to generate unique recipes!')
   }
   // Define automatable upgrade recipes for all the apotheotic gems.
   let i = 0
@@ -91,71 +91,6 @@ ServerEvents.recipes((e) => {
     })
     ++i
   }
-
-  // Thermal molten fluid components
-  create
-    .mixing(
-      [
-        Fluid.of('thermal:redstone', 500),
-        potionFluid('quark:resilience', 500),
-        Item.of('apotheosis:vial_of_extraction').withChance(0.75),
-      ],
-      [
-        Fluid.of('kubejs:molten_redstone', 1000),
-        'apotheosis:vial_of_extraction',
-      ]
-    )
-    .superheated()
-  create
-    .mixing(Fluid.of('kubejs:molten_redstone', 10), [
-      Fluid.of('thermal:redstone', 500),
-      potionFluid('quark:resilience', 500),
-      'minecraft:glowstone_dust',
-    ])
-    .superheated()
-  create
-    .SequencedAssembly('minecraft:glowstone_dust')
-    .custom('', (pre, post) => {
-      create.crushing(post, pre)
-    })
-    .custom('Next: Energize with 8000 RF', (pre, post, json) => {
-      e.recipes.create_new_age.energising(post, pre, 8000)
-    })
-    .custom('Next: Melt in a heated basin', (pre, post) => {
-      create.mixing(post, pre).heated()
-    })
-    .outputs(Fluid.of('thermal:glowstone', 250))
-  create
-    .compacting(Fluid.of('thermal:ender', 250), 'kubejs:resonant_ender_pearl')
-    .superheated()
-
-  // Thermal alloys dusts
-  e.remove({ id: 'thermal:lumium_dust_4' })
-  create.mixing('4x thermal:lumium_dust', [
-    '3x thermal:tin_dust',
-    'thermal:silver_dust',
-    Fluid.of('thermal:glowstone', 500),
-  ])
-  e.remove({ id: 'thermal:signalum_dust_4' })
-  create.mixing('4x thermal:signalum_dust', [
-    '3x thermal:copper_dust',
-    'thermal:silver_dust',
-    Fluid.of('thermal:redstone', 1000),
-  ])
-  e.remove({ id: 'thermal:enderium_dust_2' })
-  create.mixing('4x thermal:enderium_dust', [
-    '3x thermal:lead_dust',
-    'thermal:diamond_dust',
-    Fluid.of('thermal:ender', 500),
-  ])
-
-  // Redstone pearls
-  e.remove({ id: 'createteleporters:redstone_pearl_recipe' })
-  create
-    .SequencedAssembly('minecraft:ender_pearl')
-    .fill('kubejs:molten_redstone', 180)
-    .energize(40000)
-    .outputs('createteleporters:redstone_pearl')
 
   // Custom XP Crystal
   e.replaceOutput(
