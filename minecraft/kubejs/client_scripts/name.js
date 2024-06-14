089333b9d91d766a1b8e23cb09eee351ba989ea7
omgimanerd <alvin@omgimanerd.tech>
2024-06-14 02:17:14 -0400

    Fix l10n issues with some modded slime fluid and quantum fluid.

--- minecraft/kubejs/client_scripts/name.js
+++ minecraft/kubejs/client_scripts/name.js
@@ -2,4 +2,8 @@
 
 ClientEvents.lang('en_us', (e) => {
   e.add('jade.modName.kubejs', 'Create Advanced Industries')
+
+  // Missing localization from mods
+  e.add('fluid_type.create_things_and_misc.slime', 'Liquid Slime')
+  e.add('fluid_type.createteleporters.quantum_fluid', 'Quantum Fluid')
 })
