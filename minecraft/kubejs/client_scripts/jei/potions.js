bcc5e01cfb3861d3b856677506b99de9b886367a
omgimanerd <alvin@omgimanerd.tech>
2024-06-14 01:19:00 -0400

    Major refactor using dev build of kjs additions.

--- minecraft/kubejs/client_scripts/jei/potions.js
+++ minecraft/kubejs/client_scripts/jei/potions.js
@@ -2,7 +2,7 @@
 // Recategorizes the custom brewing and centrifugation recipes so that potion
 // recipes don't clutter JEI.
 
-;(() => {
+JEIAddedEvents.registerCategories((e) => {
   // The Centrifugation recipe category, contains the code that performs the
   // actual rendering of the recipe inputs and outputs in JEI
   const $CentrifugationCategory = Java.loadClass(
@@ -18,25 +18,24 @@
   const $Info = Java.loadClass(
     'com.simibubi.create.compat.jei.category.CreateRecipeCategory$Info'
   )
-  const potionCentrifugationRecipeType = $RecipeType.create(
-    'kubejs',
-    'potion_centrifuging',
-    $CentrifugationRecipe
+
+  const guiHelper = e.data.jeiHelpers.guiHelper
+  // Create a concrete instance of the centrifugation recipe category and
+  // defer our custom category to use its render code.
+  const background = guiHelper.createBlankDrawable(177, 113)
+  const centrifugationCategory = new $CentrifugationCategory(
+    // Don't actually need any of the subfields, just need an instance of
+    // CentrifugationCategory to call its lookup and draw handler.
+    new $Info(null, null, background, null, null, null)
   )
 
-  JEIAddedEvents.registerCategories((e) => {
-    const guiHelper = e.data.jeiHelpers.guiHelper
-    // Create a concrete instance of the centrifugation recipe category and
-    // defer our custom category to use its render code.
-    const centrifugationCategory = new $CentrifugationCategory(
-      // Don't actually need any of the subfields, just need an instance of
-      // CentrifugationCategory to call its lookup and draw handler.
-      new $Info(null, null, null, null, null, null)
-    )
-    e.register(potionCentrifugationRecipeType, (category) => {
+  e.wrap(
+    $RecipeType.create('kubejs', 'potion_centrifuging', $CentrifugationRecipe),
+    centrifugationCategory,
+    (category) => {
       category
         .title('Potion Centrifugation')
-        .background(guiHelper.createBlankDrawable(177, 113))
+        .background(background)
         .icon(
           doubleItemIcon(
             'vintageimprovements:centrifuge',
@@ -44,80 +43,51 @@
           )
         )
         .isRecipeHandled(() => true)
-        .handleLookup((builder, recipe, focuses) => {
-          centrifugationCategory.setRecipe(builder, recipe, focuses)
-        })
-        .setDrawHandler(
-          (recipe, recipeSlotsView, guiGraphics, mouseX, mouseY) => {
-            centrifugationCategory.draw(
-              recipe,
-              recipeSlotsView,
-              guiGraphics,
-              mouseX,
-              mouseY
-            )
-          }
-        )
+    }
+  )
+})
+
+JEIAddedEvents.onRuntimeAvailable((e) => {
+  const { recipeManager } = e.data
+
+  // Move all custom potion brewing recipes from the Create mixer category to
+  // Create automated brewing.
+  const customBrewingRecipes = recipeManager
+    .createRecipeLookup('create:mixing')
+    .get()
+    .filter((recipe) => {
+      return recipe.getId().toString().startsWith('kubejs:create_potion_mixing')
     })
-  })
+    .toList()
+  recipeManager.hideRecipes('create:mixing', customBrewingRecipes)
+  recipeManager.addRecipes('create:automatic_brewing', customBrewingRecipes)
 
-  JEIAddedEvents.onRuntimeAvailable((e) => {
-    const { recipeManager } = e.data
+  // Move all potion centrifugation recipes from the CVI category to the new
+  // custom category.
+  const customCentrifugationRecipes = recipeManager
+    .createRecipeLookup('vintageimprovements:centrifugation')
+    .get()
+    .filter((recipe) => {
+      return recipe
+        .getId()
+        .toString()
+        .startsWith('kubejs:create_potion_centrifuging')
+    })
+    .toList()
+  recipeManager.hideRecipes(
+    'vintageimprovements:centrifugation',
+    customCentrifugationRecipes
+  )
+  recipeManager.addRecipes(
+    'kubejs:potion_centrifuging',
+    customCentrifugationRecipes
+  )
+})
 
-    // Move all custom potion brewing recipes from the Create mixer category to
-    // Create automated brewing.
-    const createMixingRecipeType = recipeManager
-      .getRecipeType('create:mixing')
-      .get()
-    const createAutomaticBrewingRecipeType = recipeManager
-      .getRecipeType('create:automatic_brewing')
-      .get()
-    const customBrewingRecipes = recipeManager
-      .createRecipeLookup(createMixingRecipeType)
-      .get()
-      .filter((recipe) => {
-        return recipe
-          .getId()
-          .toString()
-          .startsWith('kubejs:create_potion_mixing')
-      })
-      .toList()
-    recipeManager.hideRecipes(createMixingRecipeType, customBrewingRecipes)
-    recipeManager.addRecipes(
-      createAutomaticBrewingRecipeType,
-      customBrewingRecipes
-    )
-
-    // Move all potion centrifugation recipes from the CVI category to the new
-    // custom category.
-    const centrifugationRecipeType = recipeManager
-      .getRecipeType('vintageimprovements:centrifugation')
-      .get()
-    const customCentrifugationRecipes = recipeManager
-      .createRecipeLookup(centrifugationRecipeType)
-      .get()
-      .filter((recipe) => {
-        return recipe
-          .getId()
-          .toString()
-          .startsWith('kubejs:create_potion_centrifuging')
-      })
-      .toList()
-    recipeManager.hideRecipes(
-      centrifugationRecipeType,
-      customCentrifugationRecipes
-    )
-    recipeManager.addRecipes(
-      potionCentrifugationRecipeType,
-      customCentrifugationRecipes
-    )
-  })
-
-  JEIAddedEvents.registerRecipeCatalysts((e) => {
-    e.data.addRecipeCatalyst(
-      'vintageimprovements:centrifuge',
-      potionCentrifugationRecipeType
-    )
-    e.data.addRecipeCatalyst('create:basin', potionCentrifugationRecipeType)
-  })
-})()
+JEIAddedEvents.registerRecipeCatalysts((e) => {
+  e.data.addRecipeCatalyst(
+    'vintageimprovements:centrifuge',
+    'kubejs:potion_centrifuging'
+  )
+  e.data.addRecipeCatalyst('create:basin', 'kubejs:potion_centrifuging')
+})
