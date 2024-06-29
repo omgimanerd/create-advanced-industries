8a3d4c4046f1c889245dfc0c82ee72087c28c40c
omgimanerd <alvin@omgimanerd.tech>
2024-06-29 03:14:57 -0400

    Add a tag to group all the custom ponders.

---
+++ minecraft/kubejs/client_scripts/ponder/ponderTags.js
@@ -0,0 +1,21 @@
+// priority: 0
+
+Ponder.tags((e) => {
+  e.createTag(
+    'kubejs:custom_mechanics',
+    'kubejs:andesite_mechanism',
+    'Custom Mechanics',
+    'All the custom crafting and interaction mechanics in this modpack',
+    [
+      'kubejs:arcane_portal',
+      'minecraft:beehive',
+      'minecraft:budding_amethyst',
+      'minecraft:dragon_head',
+      'minecraft:enchanting_table',
+      'minecraft:infested_stone',
+      'minecraft:moss_block',
+      'minecraft:note_block',
+      'kubejs:remy_spawner',
+    ]
+  )
+})
