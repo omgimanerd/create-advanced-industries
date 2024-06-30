omgimanerd/create-advanced-industries

commit bbca6e15fd03f7a29d41af24f7ac596a0c36f170

Standardized molten metal alloying recipes.

minecraft/kubejs/server_scripts/overhauls/metallurgy.js
@@ -22,15 +22,14 @@ ServerEvents.recipes((e) => {
     i.registerMeltingRecipes(e)
       .registerCastingRecipes(e)
       .registerWashedCastRecipes(e)
+      .registerDustCrushingRecipes(e)
   })
 
   // Manually register melting recipes for all glass types
-  e.recipes.create
-    .mixing(Fluid.of('kubejs:molten_glass', 360), '#forge:glass')
-    .heated()
+  create.mixing(Fluid.of('kubejs:molten_glass', 360), '#forge:glass').heated()
 
   // Better melting recipe for sand into glass
-  e.recipes.create
+  create
     .mixing(Fluid.of('kubejs:molten_glass', 450), '#minecraft:smelts_to_glass')
     .heated()
 
@@ -39,15 +38,15 @@ ServerEvents.recipes((e) => {
   const ceramicCastedMoltenGlass = 'kubejs:ceramic_ingot_cast_molten_glass'
   const steelCastedMoltenGlass = 'kubejs:steel_ingot_cast_molten_glass'
   const MeltableItem = global.MeltableItem
-  e.recipes.create.filling(ceramicCastedMoltenGlass, [
+  create.filling(ceramicCastedMoltenGlass, [
     Fluid.of('kubejs:molten_glass', MeltableItem.DEFAULT_INGOT_FLUID * 4),
     MeltableItem.CERAMIC_INGOT_CAST,
   ])
-  e.recipes.create.filling(steelCastedMoltenGlass, [
+  create.filling(steelCastedMoltenGlass, [
     Fluid.of('kubejs:molten_glass', MeltableItem.DEFAULT_INGOT_FLUID * 4),
     MeltableItem.STEEL_INGOT_CAST,
   ])
-  e.recipes.create.splashing(
+  create.splashing(
     [
       'minecraft:glass',
       Item.of(MeltableItem.CERAMIC_INGOT_CAST).withChance(
@@ -56,8 +55,34 @@ ServerEvents.recipes((e) => {
     ],
     ceramicCastedMoltenGlass
   )
-  e.recipes.create.splashing(
+  create.splashing(
     ['minecraft:glass', MeltableItem.STEEL_INGOT_CAST],
     steelCastedMoltenGlass
   )
+
+  // Register all metallic alloying recipes, and remove the custom blend
+  // crafting.
+  global.materials
+    .filter((v) => {
+      return v.type === global.MATERIAL_TYPE_ALLOY_METAL && v.alloyRatios
+    })
+    .forEach((/** @type {Material} */ v) => {
+      const { dust, fluid, superheated, alloyRatios } = v
+      e.remove({ type: 'minecraft:crafting_shapeless', output: dust })
+      let totalFluidOutput = 0
+      const ingredients = Object.entries(alloyRatios).map((e) => {
+        totalFluidOutput += e[1] * MeltableItem.DEFAULT_INGOT_FLUID
+        return Fluid.of(e[0], e[1] * MeltableItem.DEFAULT_INGOT_FLUID)
+      })
+      const fluidOutput = Fluid.of(fluid, totalFluidOutput)
+      console.log(fluidOutput, ingredients)
+      const r = create.mixing(fluidOutput, ingredients)
+      if (superheated) {
+        r.superheated()
+      } else {
+        r.heated()
+      }
+    })
+
+  // Rich slag + liquid = better?
 })

minecraft/kubejs/server_scripts/chapters/chapter3.js
@@ -33,7 +33,13 @@ ServerEvents.recipes((e) => {
     )
     .id('kubejs:custom_ars_nouveau_enchanting_blaze_burner')
 
-  // Automated dirt reward
+  // Automated dirt crafting
+  e.remove({ id: 'thermal:dirt_crafting' })
+  e.shapeless('minecraft:dirt', [
+    'thermal:compost',
+    'thermal:slag',
+    '#forge:sand',
+  ]).id('kubejs:dirt_crafting_manual_only')
   create.mixing('4x minecraft:dirt', [
     'thermal:compost',
     'thermal:slag',
@@ -100,12 +106,7 @@ ServerEvents.recipes((e) => {
 
   // Brass mixing
   e.remove({ id: 'create:mixing/brass_ingot' })
-  create
-    .mixing(Fluid.of('kubejs:molten_brass', 2 * ingotFluid), [
-      Fluid.of('kubejs:molten_copper', ingotFluid),
-      Fluid.of('kubejs:molten_zinc', ingotFluid),
-    ])
-    .heated()
+  // Overhauled in server_scripts/overhauls/metallurgy.js
 
   // Remove ars soul sand recipes
   e.remove({ id: 'ars_nouveau:conjuration_essence_to_soul_sand' })

minecraft/kubejs/server_scripts/chapters/chapter6a.js
@@ -46,26 +46,6 @@ ServerEvents.recipes((e) => {
   // Thermal alloys
   e.remove({ id: /^thermal:fire_charge.*$/ })
 
-  // Thermal alloys dusts
-  e.remove({ id: 'thermal:lumium_dust_4' })
-  create.mixing('4x thermal:lumium_dust', [
-    '3x thermal:tin_dust',
-    'thermal:silver_dust',
-    Fluid.of('thermal:glowstone', 500),
-  ])
-  e.remove({ id: 'thermal:signalum_dust_4' })
-  create.mixing('4x thermal:signalum_dust', [
-    '3x thermal:copper_dust',
-    'thermal:silver_dust',
-    Fluid.of('thermal:redstone', 1000),
-  ])
-  e.remove({ id: 'thermal:enderium_dust_2' })
-  create.mixing('4x thermal:enderium_dust', [
-    '3x thermal:lead_dust',
-    'thermal:diamond_dust',
-    Fluid.of('thermal:ender', 500),
-  ])
-
   // Redstone pearls
   e.remove({ id: 'createteleporters:redstone_pearl_recipe' })
   create

minecraft/kubejs/startup_scripts/metallurgy/materials.js
@@ -6,10 +6,7 @@
   (global.MATERIAL_TYPE_GEM = 'gem')
 
 /**
- * Holds all the mappings for every form of material for easy lookup and
- * transformation.
- *
- * @typedef {object} Material
+ * @typedef {Object} Material
  * @property {string} type
  * @property {string} block
  * @property {string} ingot
@@ -23,6 +20,11 @@
  *   generate_fluid_textures.py
  * @property {string=} fluidTextureLocation
  * @property {boolean=} superheated
+ * @property {{string:number}} alloyRatios
+ */
+/**
+ * Holds all the mappings for every form of material for easy lookup and
+ * transformation.
  *
  * @type {Material[]}
  */
@@ -157,6 +159,11 @@ global.materials = [
 
     fluid: 'kubejs:molten_brass',
     bucketColor: 0xd19c39,
+
+    alloyRatios: {
+      'kubejs:molten_copper': 1,
+      'kubejs:molten_zinc': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -180,6 +187,11 @@ global.materials = [
 
     fluid: 'kubejs:molten_invar',
     bucketColor: 0x849494,
+
+    alloyRatios: {
+      'kubejs:molten_iron': 2,
+      'kubejs:molten_nickel': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -191,6 +203,13 @@ global.materials = [
 
     fluid: 'kubejs:molten_enderium',
     bucketColor: 0x0c5c7c,
+    superheated: true,
+
+    alloyRatios: {
+      'kubejs:molten_lead': 3,
+      'kubejs:molten_diamond': 1,
+      'thermal:ender': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -202,6 +221,13 @@ global.materials = [
 
     fluid: 'kubejs:molten_signalum',
     bucketColor: 0xa20f00,
+    superheated: true,
+
+    alloyRatios: {
+      'kubejs:molten_copper': 3,
+      'kubejs:molten_silver': 1,
+      'thermal:redstone': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -213,6 +239,12 @@ global.materials = [
 
     fluid: 'kubejs:molten_constantan',
     bucketColor: 0xa46424,
+    superheated: true,
+
+    alloyRatios: {
+      'kubejs:molten_copper': 1,
+      'kubejs:molten_nickel': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -224,6 +256,12 @@ global.materials = [
 
     fluid: 'kubejs:molten_lumium',
     bucketColor: 0xfbf3c0,
+
+    alloyRatios: {
+      'kubejs:molten_tin': 3,
+      'kubejs:molten_silver': 1,
+      'thermal:glowstone': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -235,6 +273,11 @@ global.materials = [
 
     fluid: 'kubejs:molten_electrum',
     bucketColor: 0xbca44c,
+
+    alloyRatios: {
+      'kubejs:molten_gold': 1,
+      'kubejs:molten_silver': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,
@@ -246,6 +289,11 @@ global.materials = [
 
     fluid: 'kubejs:molten_bronze',
     bucketColor: 0x8c4424,
+
+    alloyRatios: {
+      'kubejs:molten_copper': 3,
+      'kubejs:molten_tin': 1,
+    },
   },
   {
     type: global.MATERIAL_TYPE_ALLOY_METAL,

commit 12162c8c4fa8dcf6351f7e9a45f7f3404e8e9612

Fix a bug introduced in bbca6e1 where an undefined dust caused the metallurgy overhaul to remove all shapeless crafting recipes.

minecraft/kubejs/server_scripts/overhauls/metallurgy.js
@@ -68,7 +68,12 @@ ServerEvents.recipes((e) => {
     })
     .forEach((/** @type {Material} */ v) => {
       const { dust, fluid, superheated, alloyRatios } = v
-      e.remove({ type: 'minecraft:crafting_shapeless', output: dust })
+      if (dust) {
+        // Only remove shapeless crafting recipes where a dust existed. Brass
+        // does not a dust, and this will remove all shapeless recipes if
+        // dust is undefined.
+        e.remove({ type: 'minecraft:crafting_shapeless', output: dust })
+      }
       let totalFluidOutput = 0
       const ingredients = Object.entries(alloyRatios).map((e) => {
         totalFluidOutput += e[1] * MeltableItem.DEFAULT_INGOT_FLUID