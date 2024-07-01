7414ae058631d363cc85559fabd4b24ff75ff3e1
omgimanerd <alvin@omgimanerd.tech>
2024-07-01 19:12:37 -0400

    Move item renaming from lang file to rename script.

--- minecraft/kubejs/client_scripts/rename.js
+++ minecraft/kubejs/client_scripts/rename.js
@@ -6,13 +6,19 @@ ClientEvents.lang('en_us', (e) => {
   e.renameItem('tfmg:plastic_sheet', 'Plastic')
 
   // Reuse the textures from Create Crafts & Additions for the Create: New Age
-  // wire spools
+  // wire spools.
   e.renameItem('createaddition:iron_wire', 'Overcharged Iron Wire')
   e.renameItem('createaddition:gold_wire', 'Overcharged Gold Wire')
+
+  // Why is this not capitalized?
+  e.renameItem('createarmory:impact_nade', 'Impact Grenade')
 
   // Rename fluid buckets to be consistent with Minecraft naming scheme.
   e.renameItem('createaddition:seed_oil_bucket', 'Seed Oil Bucket')
   e.renameItem('createaddition:bioethanol_bucket', 'Biofuel Bucket')
   e.renameItem('sliceanddice:fertilizer_bucket', 'Liquid Fertilizer Bucket')
   e.renameItem('createteleporters:quantum_fluid_bucket', 'Quantum Fluid Bucket')
+
+  // Name this a mechanism instead of a module.
+  e.renameItem('vintageimprovements:redstone_module', 'Redstone Mechanism')
 })
