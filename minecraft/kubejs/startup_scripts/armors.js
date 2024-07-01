179a7e16c73cdb0f804704dd2f597607abbae7ed
omgimanerd <alvin@omgimanerd.tech>
2024-07-01 19:21:29 -0400

    Rename item registration file. Create custom modification file to set max stack size of grenades to 16

--- minecraft/kubejs/startup_scripts/armors.js
+++ minecraft/kubejs/startup_scripts/armors.js
@@ -4,19 +4,19 @@ ItemEvents.modification((e) => {
   // Change Pneumaticcraft's armor to be on par with netherite since it
   // requires netherite armor.
   e.modify('pneumaticcraft:pneumatic_boots', (i) => {
-    i.armorProtection = 3
-    i.armorToughness = 3
+    i.setArmorProtection(3)
+    i.setArmorToughness(3)
   })
   e.modify('pneumaticcraft:pneumatic_leggings', (i) => {
-    i.armorProtection = 6
-    i.armorToughness = 3
+    i.setArmorProtection(6)
+    i.setArmorToughness(3)
   })
   e.modify('pneumaticcraft:pneumatic_chestplate', (i) => {
-    i.armorProtection = 8
-    i.armorToughness = 3
+    i.setArmorProtection(8)
+    i.setArmorToughness(3)
   })
   e.modify('pneumaticcraft:pneumatic_helmet', (i) => {
-    i.armorProtection = 3
-    i.armorToughness = 3
+    i.setArmorProtection(3)
+    i.setArmorToughness(3)
   })
 })
