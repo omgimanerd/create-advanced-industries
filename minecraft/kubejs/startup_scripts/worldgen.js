605b1593fc7c8ed008883a8f72f175b7cb9a2c53
omgimanerd <alvin@omgimanerd.tech>
2024-07-03 03:02:27 -0400

    Remove worldgen for Create: New Age thorium ore as suggested by #3

--- minecraft/kubejs/startup_scripts/worldgen.js
+++ minecraft/kubejs/startup_scripts/worldgen.js
@@ -2,3 +2,9 @@
 
 // list features
 // https://discord.com/channels/303440391124942858/1229784826559729756
+
+WorldgenEvents.remove((e) => {
+  e.removeOres((ores) => {
+    ores.blocks = ['create_new_age:thorium_ore']
+  })
+})
