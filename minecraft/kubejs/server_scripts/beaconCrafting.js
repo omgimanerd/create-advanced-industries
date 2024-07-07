015fc61cde9e3462603725d6bb0924ee21ca79c5
omgimanerd <alvin@omgimanerd.tech>
2024-07-07 03:42:53 -0400

    Progress commit on beaconCrafting, major refactors and cleanups to AABB logic.

--- minecraft/kubejs/server_scripts/beaconCrafting.js
+++ minecraft/kubejs/server_scripts/beaconCrafting.js
@@ -1,8 +1,35 @@
 // priority: 0
 
 BlockEvents.rightClicked('minecraft:beacon', (e) => {
-  const { block, level, server } = e
+  const { hand, item, block, player, level, server } = e
   if (block.entity.getBeamSections === undefined) return
+
+  // Must be kept in sync with the energizing recipes for each of these items.
+  const allowedItems = {
+    'create_new_age:overcharged_iron': {
+      result: 'minecraft:iron_ingot',
+      energy: 1000,
+    },
+    'create_new_age:overcharged_gold': {
+      result: 'minecraft:gold_ingot',
+      energy: 2000,
+    },
+    'create_new_age:overcharged_diamond': {
+      result: 'minecraft:diamond',
+      energy: 10000,
+    },
+    'gag:energized_hearthstone': {
+      result: 'gag:hearthstone',
+      energy: 20000,
+    },
+  }
+  const allowedItem = allowedItems[item.id]
+  if (hand !== 'main_hand' || allowedItem === undefined) return
+  let { result, energy } = allowedItem
+
+  // Return the uncharged item to the player.
+  item.shrink(1)
+  player.give(result)
 
   // While iterating through the beam sections, store the previous beam
   // redirector and direction.
@@ -11,19 +38,19 @@ BlockEvents.rightClicked('minecraft:beacon', (e) => {
   let direction = Direction.UP
 
   /**
-   * Helper method to perform the processing for each beacon beam's collision
-   * box. Handles checking all entities with the collision box and spawning
-   * firework particles along the length of the box.
-   * @param {Internal.AABB_} aabb
+   * Helper method to perform the processing for each block in a beacon beam's
+   * collision box. Handles checking all entities with a given block and
+   * spawning firework particles.
+   * @param {BlockPos_} block
    * @param {number[]} color
-   * @param {Internal.BlockContainerJS_} block
-   * @param {number} baseDelay
+   * @param {Internal.BlockContainerJS_} redirectorBlock
+   * @param {number} delay
    */
-  const processEntitiesInCollisionBox = (aabb, color, block, baseDelay) => {
+  const processEntitiesInBlock = (block, color, redirectorBlock, delay) => {
+    const aabb = AABB.ofBlock(block)
     // Average out the box size and use half its size to determine the
     // processing delay.
-    const volume = global.getVolume(aabb)
-    server.scheduleInTicks(baseDelay + volume / 2, () => {
+    server.scheduleInTicks(delay, () => {
       level
         .getEntitiesWithin(aabb)
         .filter((entity) => {
@@ -33,31 +60,20 @@ BlockEvents.rightClicked('minecraft:beacon', (e) => {
           if (entity.item.id === 'minecraft:glass') entity.discard()
           entity.block.popItem(Item.of('minecraft:yellow_stained_glass'))
         })
+      spawnParticles(
+        level,
+        'minecraft:firework',
+        block.getCenter(),
+        0.25,
+        25,
+        0.05,
+        true
+      )
     })
-
-    // Draw entities along the entire collision box at the correct delay.
-    // TODO this needs to take into account the beam direction.
-    let i = 0
-    for (const /** @type {BlockPos} */ blockPos of global.getBlockList(aabb)) {
-      // Wrap the scheduler in a closure to bind the value of the variables at
-      // the current iteration loop.
-      ;((blockPos_, baseDelay_, i_) => {
-        server.scheduleInTicks(baseDelay_ + i_, () => {
-          spawnParticles(
-            level,
-            'minecraft:firework',
-            blockPos_.getCenter(),
-            0.25,
-            25,
-            0.05,
-            true
-          )
-        })
-      })(blockPos, baseDelay, i++)
-    }
   }
 
-  // Counter used to draw a firework entity per tick.
+  // Delay counter in ticks, used to schedule the processing along the length
+  // of the beacon beam.
   let delay = 0
 
   const origin = new Vec3i(0, 0, 0)
@@ -70,56 +86,40 @@ BlockEvents.rightClicked('minecraft:beacon', (e) => {
       beam.offset.y,
       beam.offset.z
     )
-
-    let start, end
     // Integer coordinates represent the north-west corner of a block. The start
     // and end coordinates of the bounding box itself need to be computed
     // differently depending on which direction the beam was facing. Each
     // bounding box encapsulates the block modifying the beam and every block
     // up to the next modifying block, end exclusive.
+    let start = redirectorBlock.pos
+    let end = modifyingBlock.pos
     switch (direction) {
+      // POSITIVE axis direction
       case 'up':
-        start = redirectorBlock.pos
-        end = modifyingBlock.pos.offset(1, 0, 1)
-        break
-      case 'down':
-        start = redirectorBlock.pos.offset(0, 1, 0)
-        end = modifyingBlock.pos.offset(1, 1, 1)
-        break
-      case 'west':
-        start = redirectorBlock.pos.offset(1, 1, 1)
-        end = modifyingBlock.pos.offset(1, 0, 0)
-        break
       case 'east':
-        start = redirectorBlock.pos
-        end = modifyingBlock.offset(0, 1, 1)
-        break
-      case 'north':
-        start = redirectorBlock.pos.offset(0, 0, 1)
-        end = modifyingBlock.pos.offset(1, 1, 1)
-        break
       case 'south':
-        start = redirectorBlock.pos
-        end = modifyingBlock.pos.offset(1, 1, 0)
+        end = end.offset(direction.opposite.normal)
+        break
+      // NEGATIVE axis direction
+      case 'down':
+      case 'west':
+      case 'north':
+        start = start.offset(direction.opposite.normal)
         break
       default:
         throw new Error(`Unexpected direction ${direction}`)
     }
-    const beamCollisionBox = AABB.of(
-      start.x,
-      start.y,
-      start.z,
-      end.x,
-      end.y,
-      end.z
-    )
-    processEntitiesInCollisionBox(
-      beamCollisionBox,
-      color,
-      redirectorBlock,
-      delay
-    )
-    delay += global.getVolume(beamCollisionBox)
+    const beamCollisionBox = AABB.ofBlocks(start, end)
+    let blocksInCollisionBox = global.getBlockList(beamCollisionBox)
+    // The block list in the AABB is always sorted from lowest to highest
+    // coordinate, so the beam direction tells us what direction to iterate
+    // either forwards or backwards.
+    if (direction.axisDirection.step < 0) blocksInCollisionBox.reverse()
+    // Go through each block in the beam collision box
+    for (const block of blocksInCollisionBox) {
+      processEntitiesInBlock(block, color, redirectorBlock, delay)
+      delay++
+    }
 
     // Set the color and redirector block for the next beam.
     color = beam.getColor().map((v) => v)
@@ -132,16 +132,14 @@ BlockEvents.rightClicked('minecraft:beacon', (e) => {
     redirectorBlock.x,
     redirectorBlock.y,
     redirectorBlock.z,
-    redirectorBlock.x + 1,
+    redirectorBlock.x,
     256,
-    redirectorBlock.z + 1
+    redirectorBlock.z
   )
-  processEntitiesInCollisionBox(
-    finalCollisionBox,
-    color,
-    redirectorBlock,
-    delay
-  )
+  for (const block of global.getBlockList(finalCollisionBox)) {
+    processEntitiesInBlock(AABB.of(block), color, redirectorBlock, delay)
+    delay++
+  }
 
   block.getPlayersInRadius(48).forEach((p) => {
     // https://minecraft.fandom.com/wiki/Commands/playsound
