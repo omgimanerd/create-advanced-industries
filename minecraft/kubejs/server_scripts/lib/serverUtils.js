843f33118f791d77260281a14bb9816858afe7d6
omgimanerd <alvin@omgimanerd.tech>
2024-07-27 20:25:36 -0400

    Add a done() callback argument to serverUtils's repeat() function.

--- minecraft/kubejs/server_scripts/lib/serverUtils.js
+++ minecraft/kubejs/server_scripts/lib/serverUtils.js
@@ -6,13 +6,14 @@
  * @param {number} interval Repeat interval in ticks
  * @param {function} cb
  */
-const repeat = (server, duration, interval, cb) => {
+const repeat = (server, duration, interval, cb, done) => {
   const startTime = server.getTickCount()
   const endTime = startTime + duration
   server.scheduleRepeatingInTicks(0, (c) => {
     const currentTime = server.getTickCount()
-    cb()
+    cb(c)
     if (currentTime >= endTime) {
+      if (typeof done === 'function') done()
       c.clear()
     } else {
       c.reschedule(interval)
