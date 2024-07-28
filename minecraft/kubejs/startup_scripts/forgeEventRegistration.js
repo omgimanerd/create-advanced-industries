428ab1a138ee2c32bb74a8f7fbcd344378047c1d
omgimanerd <alvin@omgimanerd.tech>
2024-07-27 21:34:52 -0400

    Implement basis of enderman teleport inhibition.

--- minecraft/kubejs/startup_scripts/forgeEventRegistration.js
+++ minecraft/kubejs/startup_scripts/forgeEventRegistration.js
@@ -9,3 +9,36 @@ ForgeEvents.onEvent(
     }
   }
 )
+
+/**
+ * @param {Internal.EntityTeleportEvent_} e
+ */
+global.EndermanTeleportCallback = (e) => {
+  const { entity } = e
+  if (entity.type !== 'minecraft:enderman') return
+  for (const offset of BlockPos.betweenClosed(-2, -1, -2, 2, 2, 2)) {
+    let { x, y, z } = global.getXYZ(offset)
+    let block = entity.block.offset(x, y, z)
+    if (block.entity) {
+      let capability = block.entity.getCapability(
+        ForgeCapabilities.FLUID_HANDLER
+      )
+      if (!capability.isPresent()) continue
+      let fluidHandler = capability.resolve().get()
+      // Enderman will attempt to teleport many times in quick succession when
+      // hurt, so this should be kept to a modest value.
+      fluidHandler.fill(Fluid.water(1), 'execute')
+      e.setCanceled(true)
+      return
+    }
+  }
+}
+
+ForgeEvents.onEvent(
+  'net.minecraftforge.event.entity.EntityTeleportEvent',
+  (e) => {
+    if (global.EndermanTeleportCallback) {
+      global.EndermanTeleportCallback(e)
+    }
+  }
+)
--- minecraft/kubejs/startup_scripts/util/blockUtils.js
+++ minecraft/kubejs/startup_scripts/util/blockUtils.js
@@ -1,6 +1,19 @@
 // priority: 1000
 
 /**
+ * Returns the x, y, z values of the given argument as a dictionary for
+ * easy unpacking.
+ * @param {BlockPos_|Vec3d_|Vec3f_|Vec3i_} v
+ */
+global.getXYZ = (v) => {
+  return {
+    x: typeof v.x === 'function' ? v.x() : v.x,
+    y: typeof v.y === 'function' ? v.y() : v.y,
+    z: typeof v.z === 'function' ? v.z() : v.z,
+  }
+}
+
+/**
  * Generates a list of Vec3i objects containing all the integral block
  * coordinates with the given AABB, max exclusive.
  * @param {AABB_} aabb
