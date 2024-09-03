b7e19f42abf7f93d975769a3632cd52556b19247
omgimanerd <alvin@omgimanerd.tech>
2024-09-03 02:03:09 -0400

    Implement logic to drain chromatic fluid from a colored sheep.

--- minecraft/kubejs/server_scripts/chapters/chapter8_chromatic.js
+++ minecraft/kubejs/server_scripts/chapters/chapter8_chromatic.js
@@ -1,12 +1,31 @@
 // priority: 500
 
+// Logic to drain chromatic fluid from a colored sheep.
 EntityEvents.hurt((e) => {
+  const { entity } = e
   if (e.entity.type !== 'minecraft:sheep') return
+  const color = entity.nbt.getByte('Color')
+  if (color === 0) return
+  const block = entity.block
+  if (block.id !== 'create:item_drain') return
 
-  console.log(e.entity.nbt.getByte('Color'))
+  // TODO check the item the sheep is hit with.
 
-  let nbt = e.entity.nbt
-  nbt.putByte('Color', (nbt.getByte('Color') + 1) % 16)
+  // The item drain's fluid handler forbids regular insertion from other
+  // compatible fluid handler interactions. We are copying the drain's internal
+  // code that enables insertion to fill it with the fluid from an item, and
+  // then immediately disables it.
+  // ItemDrainBlockEntity.java#L242
+  const tank = block.entity.internalTank
+  tank.allowInsertion()
+  tank
+    .getPrimaryHandler()
+    .fill(Fluid.of('kubejs:chromatic_fluid', 40), 'execute')
+  tank.forbidInsertion()
+
+  // Clear the color of the sheep's wool.
+  const nbt = entity.nbt
+  nbt.putByte('Color', 0)
   e.entity.setNbt(nbt)
 })
 
