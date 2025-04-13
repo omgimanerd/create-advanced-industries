0364844daed2d81be63d64df74ac608a20c7b7cc
omgimanerd <alvin@omgimanerd.tech>
2025-04-12 21:31:43 -0400

    Minor comment edit.

--- minecraft/kubejs/client_scripts/tooltips/debug.js
+++ minecraft/kubejs/client_scripts/tooltips/debug.js
@@ -2,8 +2,7 @@
 // This file is not packaged in the final release modpack.
 
 ItemEvents.tooltip((e) => {
-  // Neat utility to display NBT in the tooltip, debug only, remove for the 1.0
-  // release.
+  // Neat utility to display NBT in the tooltip.
   e.addAdvancedToAll((item, _, text) => {
     if (item.nbt && e.alt) {
       text.add(Text.of('NBT: ').append(Text.prettyPrintNbt(item.nbt)))
